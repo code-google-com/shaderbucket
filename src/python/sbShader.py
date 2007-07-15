6c08ceb84c1bd67299c2fa60e7c3076ad1b9fac2
danbethell <danbethell@c46a73e1-4235-0410-baad-bb0c217080e8>
2007-07-15 05:43:23 +0000

    added the starts of a parser for the shader interface files.

--- src/python/sbShader.py
+++ src/python/sbShader.py
@@ -26,10 +26,40 @@ class Shader(Item):
     def __init__( self, filename="" ):
         Item.__init__(self)
         self.description = None
-        self.contents = []     
+        self.contents = []   
+        if filename!="":
+            self.load(filename)  
     def dump(self):
         print "==shader=="
         for item in self.contents:
             item.dump()            
     def addParameter(self, parameter):
         self.parameters.append(copy.deepcopy(parameter))
+    def load(self,filename):
+        parser = xml.sax.make_parser()
+        parser.setContentHandler( shaderReader(self) )
+        #parser.parse( filename )
+        
+#==============================================================================
+
+# Create a class to handle reader palette files
+class shaderReader(xml.sax.handler.ContentHandler):
+    # init palette reader
+    def __init__(self, parent):  
+        pass    
+
+    # start/end document
+    def startDocument(self):
+        pass        
+    def endDocument(self):
+        pass       
+
+    # start/end element
+    def startElement(self,name,attrs):
+        pass
+    def endElement(self,name):
+        pass
+
+    # element contents
+    def characters(self,content):
+        pass
